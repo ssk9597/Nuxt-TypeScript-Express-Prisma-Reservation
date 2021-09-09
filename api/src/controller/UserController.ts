9648588294caeddb984e0e089758b8171ae7dfca
sasaki <skyo.97.admj@gmail.com>
2021-09-09 21:12:42 +0900

    Create UserController

--- api/src/controller/UserController.ts
+++ api/src/controller/UserController.ts
@@ -6,8 +6,29 @@ const router = Router();
 
 // GET /api/users
 router.get('/', async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
+  const users = await prisma.user.findMany({
+    include: {
+      reservation: true,
+    },
+  });
   res.status(200).json({ users });
+});
+
+// POST /api/users/store
+router.post('/store', async (req: Request, res: Response) => {
+  await prisma.user.create({
+    data: {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      reservation: {
+        create: {
+          date: req.body.date,
+          time: req.body.time,
+        },
+      },
+    },
+  });
 });
 
 export default router;
