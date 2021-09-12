503517d104ca38e1efecd2cd9388df9d6a7a4eb8
sasaki <skyo.97.admj@gmail.com>
2021-09-12 15:04:29 +0900

    Component_Type_Calendar

--- frontend/components/Template/Calendar.vue
+++ frontend/components/Template/Calendar.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <Calendar />
+    <Calendar
+      :currentDate="currentDate"
+      :today="today"
+      :clickPrevMonth="prevMonth"
+      @prevMonth="prevMonth()"
+      :clickNextMonth="nextMonth"
+      @nextMonth="nextMonth()"
+      :clickChooseDate="chooseDate"
+      @chooseDate="chooseDate(date)"
+    />
     <br />
     <Time />
   </div>
@@ -8,6 +17,7 @@
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from '@nuxtjs/composition-api';
+import moment from 'moment';
 
 // components
 import Calendar from '../Atoms/Calendar.vue';
@@ -17,6 +27,35 @@ export default defineComponent({
   components: {
     Calendar,
     Time,
+  },
+  setup() {
+    // data
+    const currentDate = ref<any>(moment());
+    const today = ref<any>(moment());
+
+    // methods
+    const prevMonth = () => {
+      currentDate.value = moment(currentDate.value).subtract(1, 'month');
+    };
+
+    const nextMonth = () => {
+      currentDate.value = moment(currentDate.value).add(1, 'month');
+    };
+
+    const chooseDate = date => {
+      window.location.href = 'http://localhost:3000';
+      sessionStorage.date = date;
+    };
+
+    return {
+      // data
+      currentDate,
+      today,
+      // methods
+      prevMonth,
+      nextMonth,
+      chooseDate,
+    };
   },
 });
 </script>
