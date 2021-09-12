175e83258f9033bd4c640154bb5d54536c793820
sasaki <skyo.97.admj@gmail.com>
2021-09-12 15:44:25 +0900

    Component_Pages_Index

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="reservation-container">
-    <Heading :content="'ご希望の日時をお選びください'" />
-    <Calendar />
-    <Heading :content="'お客様の情報を入力してください'" />
-    <Form />
+  <div>
+    <Index />
   </div>
 </template>
 
@@ -11,26 +8,11 @@
 import { defineComponent } from '@nuxtjs/composition-api';
 
 // component
-import Heading from '../components/Atoms/Heading.vue';
-import Form from '../components/Template/Form.vue';
-import Calendar from '../components/Template/Calendar.vue';
+import Index from '../components/Pages/Index.vue';
 
 export default defineComponent({
   components: {
-    Heading,
-    Form,
-    Calendar,
+    Index,
   },
 });
 </script>
-
-<style lang="scss" scoped>
-.reservation {
-  &-container {
-    width: 100%;
-    background: #f7f5ee;
-    max-width: 450px;
-    margin: 0 auto;
-  }
-}
-</style>
