4f104238af54dc1c28989184621b0c7db5883725
sasaki <skyo.97.admj@gmail.com>
2021-09-14 10:59:12 +0900

    Create a controller

--- api/src/controller/ReservationController.ts
+++ api/src/controller/ReservationController.ts
@@ -1,17 +1,74 @@
-import { PrismaClient } from '@prisma/client';
 import { Router, Request, Response } from 'express';
 
-const prisma = new PrismaClient();
+// dotenv
+import dotenv from 'dotenv';
+dotenv.config();
+
 const router = Router();
+
+// google
+import { google, Auth, calendar_v3 } from 'googleapis';
+import credentials from '../credentials/credentials.json';
 
 // GET /api/reservations/:date
 router.get('/:date', async (req: Request, res: Response) => {
-  const reservations = await prisma.reservation.findMany({
-    where: {
-      date: req.params.date,
-    },
+  const date = req.params.date;
+
+  // 必要なスコープの定義
+  const scopes = [
+    'https://www.googleapis.com/auth/calendar.readonly',
+    'https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/calendar.events.readonly',
+    'https://www.googleapis.com/auth/calendar.events',
+  ];
+
+  // JWTクライアントをサービスアカウントで認証する
+  const jwtClient: Auth.JWT = new google.auth.JWT({
+    email: credentials.client_email,
+    key: credentials.private_key,
+    keyFile: undefined,
+    keyId: undefined,
+    scopes: scopes,
+    subject: undefined,
   });
-  res.status(200).json(reservations);
+
+  // カレンダー
+  const calendar: calendar_v3.Calendar = google.calendar('v3');
+
+  // JWTの認証を行ってイベントのリストを表示する
+  await calendar.events.list(
+    {
+      auth: jwtClient,
+      calendarId: process.env.CALENDAR_ID,
+      timeMax: `${date}T23:59:59.999Z`,
+      timeMin: `${date}T00:00:00.000Z`,
+      timeZone: 'Asia/Tokyo',
+    },
+    (err, calendar) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const results = calendar?.data.items;
+
+        // types
+        type Events = {
+          date: string | undefined;
+          time: string | undefined;
+        }[];
+
+        const events: Events = [];
+
+        results?.forEach((result) => {
+          events.push({
+            date: result.start?.dateTime?.substr(0, 10),
+            time: result.start?.dateTime?.substr(11, 5),
+          });
+        });
+
+        res.json(events);
+      }
+    }
+  );
 });
 
 export default router;
